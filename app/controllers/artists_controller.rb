f0159af894cc79f8b270b9b01b475a67795b77ca
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-18 13:16:29 -0700

    artist controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,30 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :destroy]
   def index
+    @artists = Artist.all
   end
 
   def show
   end
 
   def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
   end
 
   def destroy
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name)
   end
 end
